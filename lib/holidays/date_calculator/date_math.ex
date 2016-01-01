0a6ade62dc3e805e0fb10c6edf04ee26ceb1ca41
CoderDennis <dodava@gmail.com>
2016-01-01 03:00:15 -0600

    Prototype implementation for US holiday definitions and tests

--- lib/holidays/date_calculator/date_math.ex
+++ lib/holidays/date_calculator/date_math.ex
@@ -14,4 +14,42 @@ defmodule Holidays.DateCalculator.DateMath do
   def add_days(date, days) do
     :calendar.gregorian_days_to_date(:calendar.date_to_gregorian_days(date) + days)
   end
+
+  @doc """
+  Returns the day number for the `wday` in week number `week` for the given
+  `year` and `month`.
+
+  ## Examples
+
+      # The second Tuesday of June, 2013
+      iex> Holidays.DateCalculator.DateMath.calculate_mday(2013, 6, 2, 2)
+      {2013, 6, 11}
+
+      # The third Friday of December, 2013
+      iex> Holidays.DateCalculator.DateMath.calculate_mday(2013, 12, 3, 5)
+      {2013, 12, 20}
+
+      # The last Saturday of January, 2013
+      iex> Holidays.DateCalculator.DateMath.calculate_mday(2013, 1, :last, 6)
+      {2013, 1, 26}
+
+  """
+  def calculate_mday(year, month, :last, wday) do
+    offset = :calendar.last_day_of_the_month(year, month) - 6
+    do_calculate_mday(year, month, offset, wday)
+  end
+  def calculate_mday(year, month, week, wday) do
+    offset = (week - 1) * 7 + 1
+    do_calculate_mday(year, month, offset, wday)
+  end
+
+  defp do_calculate_mday(year, month, offset, wday) do
+    day = wday - :calendar.day_of_the_week(year, month, offset) + offset
+    correct_offset(year, month, day, offset)
+  end
+
+  defp correct_offset(year, month, day, offset) when day < offset do
+    {year, month, day + 7}
+  end
+  defp correct_offset(year, month, day, _offset), do: {year, month, day}
 end
--- test/holidays/us_test.exs
+++ test/holidays/us_test.exs
@@ -15,20 +15,26 @@ defmodule Holidays.UsTest do
                                           :us)
   end
 
-# Date.civil(2008,2,18) => 'Presidents\' Day',
-# Date.civil(2008,5,26) => 'Memorial Day',
-# Date.civil(2008,7,4) => 'Independence Day',
-# Date.civil(2008,9,1) => 'Labor Day',
-# Date.civil(2008,10,13) => 'Columbus Day',
-# Date.civil(2008,11,11) => 'Veterans Day',
-# Date.civil(2008,11,27) => 'Thanksgiving',
-# Date.civil(2013,11,28) => 'Thanksgiving',
-# Date.civil(2008,12,25) => 'Christmas Day'}.each do |date, name|
-#  assert_equal name, (Holidays.on(date, :us)[0] || {})[:name]
-# end
-# {Date.civil(2008, 3, 31) => 'Cesar Chavez Day',
-# Date.civil(2008, 11, 28) => 'Day after Thanksgiving',
-# Date.civil(2013, 11, 29) => 'Day after Thanksgiving'}.each do |date, name|
+  test "US Holidays" do
+    [{{2008, 2,18}, "Presidents' Day"},
+     # {{2008, 5,26}, "Memorial Day"},
+     {{2008, 7, 4}, "Independence Day"},
+     {{2008, 9, 1}, "Labor Day"},
+     {{2008,10,13}, "Columbus Day"},
+     {{2008,11,11}, "Veterans Day"},
+     {{2008,11,27}, "Thanksgiving"},
+     {{2008,11,28}, "Day after Thanksgiving"},
+     {{2013,11,28}, "Thanksgiving"},
+     {{2013,11,29}, "Day after Thanksgiving"},
+     {{2008,12,25}, "Christmas Day"},
+     {{2016, 3,27}, "Easter Sunday"},
+     {{2016, 3,25}, "Good Friday"}
+    ]
+    |> Enum.each(fn {date, holiday_name}
+      -> TestHelper.date_has_holiday_in_region(date, holiday_name, :us) end)
+  end
+
+# {Date.civil(2008, 3, 31) => 'Cesar Chavez Day'}.each do |date, name|
 #  assert_equal name, (Holidays.on(date, :us, :us_ca)[0] || {})[:name]
 # end
 
