0ae3253a453f99361a459ba588f1d3808d884d23
CoderDennis <dodava@gmail.com>
2017-07-01 18:03:25 -0500

    published version 0.2.2 - adds German holiday definitions

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Holidays.Mixfile do
 
   def project do
     [app: :holidays,
-     version: "0.2.1",
+     version: "0.2.2",
      elixir: "~> 1.2",
      description: "Application for finding which holidays fall on given dates.",
      name: "Holidays",
